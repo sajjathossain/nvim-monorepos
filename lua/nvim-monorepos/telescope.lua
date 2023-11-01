75d1a45b19dc26105ddcec84613ec5582b7bcf60
sajjathossain <sajjathossain.official@gmail.com>
2023-11-02 00:52:22 +0600

    updated telescope module | added entry marker support

--- lua/nvim-monorepos/telescope.lua
+++ lua/nvim-monorepos/telescope.lua
@@ -2,14 +2,31 @@
 return function(directories_with_files)
   local finders = require "telescope.finders"
   local pickers = require "telescope.pickers"
-  local conf = require("telescope.config").values
   local sorters = require("telescope.sorters")
+  local entry_marker = function(entry)
+    return {
+      value = entry,
+      display = entry[1],
+      ordinal = entry[1],
+    }
+  end
+
+  local utils = require("nvim-monorepos.utils")
+  local get_last_part_of_directory = utils.get_last_part_of_directory
+
+  local M = {}
+
+  for _, value in ipairs(directories_with_files) do
+    local key = get_last_part_of_directory(value)
+    table.insert(M, { key, value })
+  end
 
 
   pickers.new({}, {
     prompt_title = "Projects",
     finder = finders.new_table {
-      results = directories_with_files,
+      results = M,
+      entry_marker = entry_marker
     },
     sorter = sorters.get_generic_fuzzy_sorter({}),
     previewer = false,
