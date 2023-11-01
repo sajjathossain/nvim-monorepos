48095b804c7cc376c0d246824566e71e2ba43c2a
sajjathossain <sajjathossain.official@gmail.com>
2023-11-02 00:42:03 +0600

    test: updated results array

--- lua/nvim-monorepos/telescope.lua
+++ lua/nvim-monorepos/telescope.lua
@@ -5,15 +5,11 @@ return function(directories_with_files)
   local conf = require("telescope.config").values
   local sorters = require("telescope.sorters")
 
-  local data = {
-    { ordinal = 'a', display = 'a', value = 'a' },
-    { ordinal = 'b', display = 'b', value = 'b' },
-  }
 
   pickers.new({}, {
     prompt_title = "Projects",
     finder = finders.new_table {
-      results = data,
+      results = directories_with_files,
     },
     sorter = sorters.get_generic_fuzzy_sorter({}),
     previewer = false,
