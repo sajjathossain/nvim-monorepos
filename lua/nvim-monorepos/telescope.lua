99accd9314ddcb1b0d8390278a0b64a4651249a0
sajjathossain <sajjathossain.official@gmail.com>
2023-11-02 01:52:38 +0600

    updated table

--- lua/nvim-monorepos/telescope.lua
+++ lua/nvim-monorepos/telescope.lua
@@ -15,7 +15,7 @@ return function(directories_with_files)
   for _, value in ipairs(directories_with_files) do
     local key = get_last_part_of_directory(value)
     -- table.insert(M, { key, value })
-    table.insert(M, { display = key, value = key, ordinal = value })
+    table.insert(M, { key, value })
   end
 
   local enter = function(prompt_bufnr)
@@ -34,14 +34,14 @@ return function(directories_with_files)
     prompt_title = "Projects",
     finder = finders.new_table {
       results = M,
-      --   entry_maker = function(entry)
-      --     local maker = {
-      --       value = entry,
-      --       display = entry[1],
-      --       ordinal = entry[1],
-      --     }
-      --     return maker
-      --   end
+      entry_maker = function(entry)
+        local maker = {
+          value = entry,
+          display = entry[2],
+          ordinal = entry[1],
+        }
+        return maker
+      end
     },
     sorter = sorters.get_generic_fuzzy_sorter({}),
     previewer = false,
