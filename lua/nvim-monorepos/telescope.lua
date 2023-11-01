06f8f290fb60bdf444ff5b5bfe2952bca60e835e
sajjathossain <sajjathossain.official@gmail.com>
2023-11-02 00:23:27 +0600

    rewrote telescope logic

--- lua/nvim-monorepos/telescope.lua
+++ lua/nvim-monorepos/telescope.lua
@@ -1,31 +1,17 @@
 -- shows the outupt in telescope
 return function(directories_with_files)
-  -- Convert the results to a format that Telescope.nvim can handle using finders.new_table
-  local results = require("telescope.finders").new_table {
-    results = directories_with_files,
-  }
+  local finders = require "telescope.finders"
+  local pickers = require "telescope.pickers"
+  local conf = require("telescope.config").values
+  local a = function(e) return { ordinal = e[1], display = e[1], value = e[1] } end
 
-  -- Load Telescope.nvim and use the select function to display the results using the selector
-  require("telescope.builtin").find_files {
-    prompt_title = "Directories with Matching Files",
-    entry_maker = function(entry)
-      return {
-        value = entry,
-        ordinal = entry,
-        display = entry,
-      }
-    end,
-    sorter = require("telescope.sorters").get_fzy_sorter(),
-    attach_mappings = function(_, map)
-      map("i", "<CR>", function(prompt_bufnr)
-        local selection = require("telescope.actions.state").get_selected_entry()
-        require("telescope.actions").close(prompt_bufnr)
-        -- Handle the selected directory (e.g., navigate to it)
-        -- You can customize this part as needed
-        vim.cmd("cd " .. selection.value)
-      end)
-      return true
-    end,
-    results = results,
-  }
+  pickers.new({}, {
+    prompt_title = "Test",
+    finder = finders.new_table {
+      results = directories_with_files,
+      entry_maker = a,
+    },
+    sorter = conf.generic_sorter({}),
+    previewer = false,
+  }):find()
 end
