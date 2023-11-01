55c29217cd54838b0103c2cad7c88513f44002fe
sajjathossain <sajjathossain.official@gmail.com>
2023-11-02 00:36:29 +0600

    test: telescope data render

--- lua/nvim-monorepos/telescope.lua
+++ lua/nvim-monorepos/telescope.lua
@@ -3,12 +3,14 @@ return function(directories_with_files)
   local finders = require "telescope.finders"
   local pickers = require "telescope.pickers"
   local conf = require("telescope.config").values
+  local data = { { 'this' }, { 'is' }, { 'a' }, { 'test' } }
+  local a = function(e) return { ordinal = e[1], display = e[1], value = e[1] } end
 
   pickers.new({}, {
     prompt_title = "Projects",
     finder = finders.new_table {
-      results = directories_with_files,
-      entry_maker = function(x) return x end,
+      results = data,
+      entry_maker = a,
     },
     sorter = conf.generic_sorter({}),
     previewer = true,
