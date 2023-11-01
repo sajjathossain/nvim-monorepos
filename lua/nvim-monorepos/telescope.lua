6beb2813be6fb9a06fd5abe03b00a4be77b515d0
sajjathossain <sajjathossain.official@gmail.com>
2023-11-02 00:34:52 +0600

    updated telescope entry maker logic

--- lua/nvim-monorepos/telescope.lua
+++ lua/nvim-monorepos/telescope.lua
@@ -3,15 +3,14 @@ return function(directories_with_files)
   local finders = require "telescope.finders"
   local pickers = require "telescope.pickers"
   local conf = require("telescope.config").values
-  local a = function(e) return { ordinal = e[1], display = e[1], value = e[1] } end
 
   pickers.new({}, {
-    prompt_title = "Test",
+    prompt_title = "Projects",
     finder = finders.new_table {
       results = directories_with_files,
-      entry_maker = a,
+      entry_maker = function(x) return x end,
     },
     sorter = conf.generic_sorter({}),
-    previewer = false,
+    previewer = true,
   }):find()
 end
